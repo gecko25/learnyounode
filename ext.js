d732fda2b45afd6030d28bbdfc26e7aca47861f7
gecko25 <sara.tankard@gmail.com>
2015-07-26 16:08:15 -0400

    FINAL SOLUTION; handles error from readdir async method; cleanup code; final solution

--- ext.js
+++ ext.js
@@ -2,43 +2,71 @@
 var fs = require('fs');
 var path = require('path');
 
-module.exports.getListOfFilteredFiles =function (dirname, ext, callback){
+//Q: best convention to name all these callbacks and functions? 
+//this caused an infinite loop before by mixing them up
+module.exports = function (dirname, ext, maincallback){
 	var listOfAllFiles;
 	var listOfFilteredFiles = [];
-	var providedExt = ext;
+	var providedExt;
 	var err=null;
 	/* First, do the logic stuff */
 
-	//get the listOfAllFiles from directory
-	listOfAllFiles = fs.readdirSync(dirname);  //was unable to use readdir(dirname, callback) --> RangeError: max call statck size
-			
+	fs.readdir(dirname, function read_dir_callback(err, listOfAllFiles){
+		if (err){
+			return maincallback(err, null);
+		}
+
+		if (!(ext.match(/^\./g))){   //if the first char (^) is not  a ".", add it (ie html--> .html)
+			providedExt = "." + ext; 
+		};
+
+
+		//Q: the official answer didnt have a try/catch, 
+		//but would this be appropriate or overkill?
 		try{	
-		//loop thru each file	
 			if (listOfAllFiles.length===0){ throw "There are no files in this directory" };
-
-			//console.log("listOfAllFiles.length:" + listOfAllFiles.length); debug statement
-
+			 
 			 for (var i=0;i<listOfAllFiles.length;i++){
 				var filename = listOfAllFiles[i];
-				//console.log("filename:" + filename); //debug statement
-				//console.log("providedExt:" + providedExt); //debug statement
-
 				var currentExt = path.extname(filename);
-				//console.log("currentExt:" + currentExt); //debug statement
 
-				//check if it matches
+				//check if it matches 
 				if (currentExt.localeCompare(providedExt)===0)
-				{
-					//add file to array
 					listOfFilteredFiles.push(filename);
-				}
+				
 			}//end for-loop
 		}catch(e){
-			console.log("There was an error: " + e.message);
 			err = e;
 		}finally{
-			/* Second, call the callback  */
-			return callback(err, listOfFilteredFiles);
+			return maincallback(err, listOfFilteredFiles);
 		} 
-	//}); //end of fs.readdir callback
+	});		
 }//end of module function getListOfFilteredFiles
+
+
+
+/* * * * * * * * * * *
+ * OFFICIAL SOLUTION *
+ * * * * * * * * * * * 
+--> check out the cool filter method on the list array <--
+--loops thru array and uses the element as an argument for function
+--if function returns true, the element stays, if not its popped (spliced?)
+ 
+ var fs = require('fs')
+    var path = require('path')
+
+    module.exports = function (dir, filterStr, callback) {
+
+      fs.readdir(dir, function (err, list) {
+        if (err)
+          return callback(err)
+
+        list = list.filter(function (file) {
+          return path.extname(file) === '.' + filterStr
+        })
+
+        callback(null, list)
+      })
+    }
+
+*/
--- main.js
+++ main.js
@@ -4,18 +4,40 @@ var ext = require('./ext');
 //vars
 var listOfFiles = [];
 var dirname = process.argv[2];
-var providedExt = "." + process.argv[3];
+var providedExt = process.argv[3];
 
-ext.getListOfFilteredFiles(dirname, providedExt, function callback(err, listOfFilteredFiles){
+ext(dirname, providedExt, function callback(err, listOfFilteredFiles){
 
-	if (err==null){
-		//loop thru list of returned array
-		for (var i=0; i<listOfFilteredFiles.length; i++){
-			console.log(listOfFilteredFiles[i]);
-		}
+	if (err){
+		console.error("There was an error", err);
 	}
 	else{
-		console.log(err);
+		//loop thru list of returned array
+		listOfFilteredFiles.forEach(function(file){
+			console.log(file);
+		})
+		// for (var i=0; i<listOfFilteredFiles.length; i++){
+		// 	console.log(listOfFilteredFiles[i]);
+		// }		
 	}
-
 });
+
+/* * * * * * * * * * *
+ * OFFICIAL SOLUTION *
+ * * * * * * * * * * * 
+ --> Notice the console.error functionality -- we need to take advantage of this
+ --> Get comfortable with for/each loops in js
+
+    var filterFn = require('./solution_filter.js')
+    var dir = process.argv[2]
+    var filterStr = process.argv[3]
+
+    filterFn(dir, filterStr, function (err, list) {
+      if (err)
+        return console.error('There was an error:', err)
+
+      list.forEach(function (file) {
+        console.log(file)
+      })
+    })
+*/

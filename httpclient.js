b5071c277dcdf2957d9dfbb2d94e39ef17795750
gecko25 <sara.tankard@gmail.com>
2015-07-28 18:41:36 -0400

    Added solution for httpclient exercise

--- httpclient.js
+++ httpclient.js
@@ -4,14 +4,18 @@ var url = process.argv[2];
 http.get(url, function callback(response){
 		//Q: Why is this callback signature different?
 		//Becuase any error information is contained in the response object? 
-		//We dont need a specific err object?
+		//We dont need a specific err object? --> YES (see official solution below)
 
 		//The on method returns a buffer object. 
 		//We need to set encoding to utf8 so that is prints readable strings
 		response.setEncoding('utf8');
 
-		response.on('data', function(chunk) {			
-  			console.log(chunk);
+		response.on('data', function(data) {			
+  			console.log(data);
+		});
+
+		response.on('error', function(error) {			
+  			console.log("There was an error", error);
 		});
 });
 
@@ -31,7 +35,7 @@ emit events are instances of events.EventEmitter. (1)
 - An http response to a server is a stream object (it "implements"/links 
 to the stream object)
 
-events<--stream<--http  (remember behavior delegation is bottom up!)
+events<--stream<--http<--response  (remember behavior delegation is bottom up!)
 
 *1* https://nodejs.org/api/events.html#events_emitter_on_event_listener
 *2* https://nodejs.org/api/stream.html#stream_stream

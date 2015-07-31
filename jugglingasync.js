4eea502c0b8f95169135e3ab43a52545a1cef974
gecko25 <sara.tankard@gmail.com>
2015-07-31 10:43:21 -0400

    adding solution to jugglingasync.js

--- jugglingasync.js
+++ jugglingasync.js
@@ -5,32 +5,76 @@ var bufferedlist = require('bl');
 
 //vars
 var urls = getUrls(process.argv);
-console.log(urls);
+var queue = [];
 
 //callbacks
-
 urls.forEach(function(url){
+		http.get(url, function(response){ //outer callback
+			
+			var strReceived = [];
+			response.setEncoding('utf8');
+			
+			response.on('data', function(data){ //inner callback
+				strReceived.push(data);
+			});
 
-		http.get(url, function(response){
-			response.pipe(bufferedlist(function(err,buffer){		
-				console.log(url);
-				console.log(buffer.toString());
-				}) //end bufferedlist()
-			)//end response.pipe		
+			response.on('end', function(){
+				//console.log("received end event.. the url is " + url);
+				var output = {url: url,
+							  data: strReceived.join("")
+							 }	 
+							 
+				//handle incoming URL
+				if (urls.length>0){
+					
+					if (output.url==urls[0]){ //urls match
+						console.log(output.data); //print
+						urls.shift(); //remove 1st element in urls array
+						
+						if (queue.length>0) //check queue with new urls[]
+							queue = checkqueue(queue, urls);
+					}
+
+					else{ //urls don't match, add to queue
+						queue.push(output);	
+					}
+			}
 		});
-});
-
-
-
-
+	});
+});		
 
 /******************/
 /*Helper functions*/
 /******************/
-/*
- *Function to get the 3 urls
- *@param - process.argv[]
- *@return - array with urls passed
+
+/**Function to search queue
+ * @param queue -- array of objects. Each object represents the url
+ * @param urls  -- array of urls that was used to request data.
+ *			       the index corresponds to the order to be printed
+ * 				   (urls[0] is first, urls[1] is second, etc.)
+*/
+function checkqueue(queue, urls){
+	//does queue contain the newly shifted urls[0]	
+	var i=0;
+	if (queue.length>0 && queue[i].url===urls[0]){
+		//something in the queue matched, print to console
+		console.log(queue[i].data);
+
+		//remove from queue
+		queue.splice(i,1);
+
+		//remove that url from array
+		urls.shift();
+
+		//**Search queue again with updated queue and urls[]**//
+		checkqueue(queue,urls);
+	}
+	return queue;									
+}
+
+/**Function to get the 3 urls**
+ *@param args   -- process.argv[]
+ *@return urls  -- array with urls passed
  */
 
 function getUrls(args){

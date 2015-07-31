0b92657c574215b59098397334b8a51e1bcbff5d
gecko25 <sara.tankard@gmail.com>
2015-07-31 10:51:21 -0400

    adding official solution in comments

--- jugglingasync.js
+++ jugglingasync.js
@@ -1,5 +1,4 @@
 //requires
-
 var http = require('http');
 var bufferedlist = require('bl');
 
@@ -84,3 +83,51 @@ function getUrls(args){
 	}
 	return urls;
 }
+
+
+
+
+
+
+
+/*****************
+OFFICIAL SOLUTION
+*****************
+
+    var http = require('http')
+    var bl = require('bl')
+    var results = []
+    var count = 0
+
+    function printResults () {
+      for (var i = 0; i < 3; i++)
+        console.log(results[i])
+    }
+
+    function httpGet (index) {
+      http.get(process.argv[2 + index], function (response) {
+        response.pipe(bl(function (err, data) {
+          if (err)
+            return console.error(err)
+
+          results[index] = data.toString()
+          count++
+
+          if (count == 3)
+            printResults()
+        }))
+      })
+    }
+
+    for (var i = 0; i < 3; i++)
+      httpGet(i)
+
+*****
+Notes
+*****
+You had tried a similar methodology, but without
+creating the additional httpGet function. You
+had only tried to use a for loop, and because this
+didn't provide a new scope, it didn't work. 
+
+*/    
